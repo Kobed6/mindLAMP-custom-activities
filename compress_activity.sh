10d16a4c1221295fb950e655f643508150ffda75
Kobe Du <kobe.du@stonybrook.edu>
2026-03-30 01:34:56 -0400

    changed compress_activity to output files to correct directories with correct names

--- compress_activity.sh
+++ compress_activity.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-npx --yes --package inline-source-cli inline-source --attribute "" --compress false --root ./dist ./dist/index.html > dist.html
-openssl base64 -in dist.html -out dist.html.b64
+npx --yes --package inline-source-cli inline-source --attribute "" --compress false --root ./dist ./dist/index.html > dist/in/reaction.html
+openssl base64 -in dist/in/reaction.html -out dist/out/reaction.html.b64
